e8e42df427235083d5390b74646cb24030481512
shahk-dev <shaxriyorturgunov000@gmail.com>
2026-01-06 04:56:17 +0500

    String method and Set lessons

---
+++ Set.py
@@ -0,0 +1,5 @@
+setA = {1 ,2 , 3 , 4, 5 , 6}
+setB = {1 , 2 , 3}
+print(setA.issuperset(setA))
+
+
---
+++ String.py
@@ -0,0 +1,17 @@
+var = 3.435453
+full = "Hello John"
+my_string = "the varibles is %s" % full
+mystrings = "the varibles is %.3f" % var
+print(mystrings)
+print(my_string)
+
+print("--------------------------------------------------")
+
+my_format = "the varible is {}".format(full)
+my_formats = "the varible if {:.2f}".format(var)
+print(my_formats)
+print(my_format)
+
+
+print("--------------------------------------------------")
+

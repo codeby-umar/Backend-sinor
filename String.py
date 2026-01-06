6ea2436683e84f952acf99f7da2d5780f6221797
shahk-dev <shaxriyorturgunov000@gmail.com>
2026-01-06 05:46:26 +0500

    algoritm masala

--- String.py
+++ String.py
@@ -15,3 +15,68 @@ print(my_format)
 
 print("--------------------------------------------------")
 
+
+#01 Homework
+
+title = "python      hello John , How are you doing today ? python is fun"
+jami = title.strip()
+jami = jami.lower()
+jami = title.find("python")
+jami = title.count("python")
+print(jami)
+
+
+print("--------------------------------------------------")
+
+#02 Homework
+
+number = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "10"]
+run = number.append("11")
+print(run)
+
+
+#03 Homework
+
+# t = (10 , 20 , 30 , 40 , 50)
+# t['0'] = 200
+# print(t)  # tuble o'zgaritilmaydi
+
+
+#04 Homewrok
+
+por = "Hello world"
+
+for i in por:
+    set(i)
+    print(set(i))
+    continue
+
+
+#05 Homework
+
+n = [1 , 2  , 3 , 4 , 5 , 10]
+jami = sum(n)
+print('Jami somma is : %i' % jami)
+
+
+
+#06 Homework
+
+number = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+total = 0
+i = 0
+
+while i < len(number):
+    total += number[i]
+    i += 1
+print(total)
+
+
+if i < len(number):
+    total += number[i]
+    i += 1
+
+print(f"Total is : {total}")
+
+
+
